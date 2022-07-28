defd28aa6b561427445ecdd959baa7100fcd3361
nakta <nakta0511@gmail.com>
2022-07-28 15:12:08 +0900

    item modify page and controller

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -2,13 +2,13 @@ package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.Item.Book;
 import jpabook.jpashop.domain.Item.Item;
+import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -18,6 +18,7 @@ import java.util.List;
 public class ItemController {
 
     private final ItemService itemService;
+    private final ItemRepository itemRepository;
 
     @GetMapping("/items/new")
     public String createForm(Model model) {
@@ -48,5 +49,37 @@ public class ItemController {
         List<Item> items = itemService.viewAllItem();
         model.addAttribute("items", items);
         return "items/itemList";
+    }
+
+    @GetMapping("/items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+        Book item = (Book)itemRepository.findById(itemId);
+
+        BookForm itemForm = new BookForm();
+        itemForm.setId(item.getId());
+        itemForm.setName(item.getName());
+        itemForm.setPrice(item.getPrice());
+        itemForm.setStockQuantity(item.getStockQuantity());
+        itemForm.setAuthor(item.getAuthor());
+        itemForm.setIsbn(item.getIsbn());
+
+        model.addAttribute("itemForm", itemForm);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping ("/items/{itemId}/edit")
+    public String updateItem(@ModelAttribute("itemForm") BookForm form) {
+
+        Book book = new Book();
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+
+        return "redirect:/items/list";
     }
 }
